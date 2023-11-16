f771872aa53964d69c89d89e2e2c9271425fb176
Saulo <saulo.assef98@gmail.com>
2023-11-16 14:43:11 -0300

    feat: Styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,34 @@ function App() {
     <div>
       <main className='container'>
         <img
+          className='logo'
           src={logoImg}
           alt='Logo da calculadora de gasolina ou etanol'></img>
       </main>
+      <h1 className='title'> Qual melhor opção?</h1>
+
+      <form className='form'>
+        <label>Etanol (preço por litro):</label>
+        <input
+          className='input'
+          type='number'
+          placeholder='4,90'
+          min='1'
+          step='0.01'
+          required
+        />
+        <label>Gasolina (preço por litro):</label>
+        <input
+          className='input'
+          type='number'
+          placeholder='4,90'
+          min='1'
+          step='0.01'
+          required
+        />
+
+        <input type='submit' className='button' value='Calcular' />
+      </form>
     </div>
   );
 }
